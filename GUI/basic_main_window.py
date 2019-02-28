1b577ff818d13ad7d0926cc5d966f9e73efbd964
Alexandre Marcotte <alexandre.marcotte.1094@gmail.com>
2019-02-28 12:06:07 -0500

    Basic example of a pyqtgraph plot inside a PyQt Window

--- GUI/basic_main_window.py
+++ GUI/basic_main_window.py
@@ -11,42 +11,22 @@ class MainWin(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.table_widget = TabWidget()
-        self.setCentralWidget(self.table_widget)
+        self.plot_widget = PlotWidget()
+        self.setCentralWidget(self.plot_widget)
         self.show()
 
 
-class ScrollPlot(QWidget):
+class PlotWidget(QWidget):
     def __init__(self):
         super().__init__()
-        # Create the tab itself
-        self.create_tab()
-
-    def create_tab(self):
-        l = QGridLayout(self)
-        q = deque(np.random.random(1000), maxlen=1000)
-        # p = pg.plot(q)
-        p_w = pg.PlotWidget()
-        self.curve = p_w.plot(q)
-        p = self.curve.setData(q)
-        l.addWidget(p)
-        # self.l.addWidget(self.plot)
-        self.setLayout(l)
-
-
-class TabWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.layout = QVBoxLayout(self)
-        self.tabs = QTabWidget()
-        self.tabs.addTab(ScrollPlot(), 'Scroll plot')
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
+        layout = QGridLayout(self)
+        q = deque(np.random.random(100), maxlen=100)
+        plot = pg.PlotWidget()
+        layout.addWidget(plot)
+        plot.plot(q)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWin()
-    main_window.show()
+    m = MainWin()
     sys.exit(app.exec_())
-
